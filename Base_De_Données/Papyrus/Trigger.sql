4c9ed9c3d265b50aa0132704c0e1fb3acc78818a
Jarod Ducamp <ducamp.jarod3011@gmail.com>
2023-03-10 11:41:24 +0100

    Debut Trigger

---
+++ Base_De_Données/Papyrus/Trigger.sql
@@ -0,0 +1,17 @@
+--Exemple--
+
+DELIMITER $$
+CREATE OR REPLACE TRIGGER insert_station AFTER INSERT ON station
+    FOR EACH ROW
+    BEGIN
+        DECLARE altitude INT;
+        SET altitude = NEW.sta_altitude;
+        IF altitude<1000 THEN
+            SIGNAL SQLSTATE '40000' SET MESSAGE_TEXT = 'Un problème est survenu. Règle de gestion altitude !';
+     END IF;
+END $$
+
+
+
+--Exo 1--
+
