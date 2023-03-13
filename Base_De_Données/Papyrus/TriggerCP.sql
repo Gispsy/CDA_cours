60223d5e3d40eadb17b50b54a13e1679e377d857
Jarod Ducamp <ducamp.jarod3011@gmail.com>
2023-03-13 16:49:14 +0100

    declencheur debut presque fin

---
+++ Base_De_Données/Papyrus/TriggerCP.sql
@@ -0,0 +1,44 @@
+DELIMITER $$
+CREATE TRIGGER maj_total AFTER INSERT ON lignedecommande
+    FOR EACH ROW
+    BEGIN
+        DECLARE id_c INT;
+        DECLARE tot DOUBLE;
+        SET id_c = NEW.id_commande;
+        SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c);
+        UPDATE commande SET total=tot WHERE id=id_c;
+END $$
+
+
+--Exo 1--
+
+INSERT INTO lignedecommande(id_commande, id_produit, quantite, prix)
+VALUES
+(1, 1, 3, 10);
+
+--Exo 2--
+
+DELIMITER $$
+CREATE TRIGGER maj_UP AFTER UPDATE ON lignedecommande
+    FOR EACH ROW
+    BEGIN
+        DECLARE id_c INT;
+        DECLARE tot DOUBLE;
+        SET id_c = NEW.id_commande;
+        SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c);
+        UPDATE commande SET total=tot WHERE id=id_c;
+END $$
+
+DELIMITER $$
+CREATE TRIGGER maj_DEL AFTER DELETE ON lignedecommande
+    FOR EACH ROW
+    BEGIN
+        DECLARE id_c INT;
+        DECLARE tot DOUBLE;
+        SET id_c = OLD.id_commande;
+        SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c);
+        UPDATE commande SET total=tot WHERE id=id_c;
+END $$
+
+DELETE FROM produit
+WHERE
